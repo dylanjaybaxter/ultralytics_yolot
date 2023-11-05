45afd37571511f006f2da3edf1abc1ed7c34881d
dylanjaybaxter <dylanjaybaxter@gmail.com>
2023-11-04 17:53:42 -0700

    Creating run managment and ensuring path existance for model load

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -106,9 +106,9 @@ class SequenceValidator():
                         pred_cls.append(torch.tensor(cls).to(self.device))
                         pred_scores.append(torch.tensor(score).to(self.device))
                 preds.append({
-                    'boxes':torch.clip(torch.stack(pred_boxes, dim=0), min=0, max=1280).unsqueeze(0),
-                    'labels':torch.stack(pred_cls, dim=0).to(torch.int).unsqueeze(0),
-                    'scores':torch.stack(pred_scores).unsqueeze(0)
+                    'boxes':torch.clip(torch.stack(pred_boxes, dim=0), min=0, max=1280),
+                    'labels':torch.stack(pred_cls, dim=0).to(torch.int),
+                    'scores':torch.stack(pred_scores)
                 })
 
             #print(f"Targets({len(targets)}): boxes-{targets[0]['boxes'].shape}, labels-{targets[0]['labels']}")
