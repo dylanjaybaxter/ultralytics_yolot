a704061458fa556eef1094c49448282fde02ef1d
dylanjaybaxter <dylanjaybaxter@gmail.com>
2023-11-05 13:17:04 -0800

    changing to targets/targets to confirm preds are problem

--- ultralytics/models/yolo/detect/val.py
+++ ultralytics/models/yolo/detect/val.py
@@ -84,7 +84,7 @@ class SequenceValidator():
                         target_classes.append(cls)
                 targets.append({
                     'boxes':sequence[0]['bboxes'][sequence[0]['frame_idx'] == i, :],
-                    'labels':sequence[0]['cls'][sequence[0]['frame_idx'] == i]
+                    'labels':sequence[0]['cls'][sequence[0]['frame_idx'] == i].squeeze()
                 })
 
                 # Get predicted boxes
