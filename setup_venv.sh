fe0e277637e2d65bd8604f76c35a0cc5b2803bae
Dylan Baxter <dylanjaybaxter@gmail.com>
2024-02-07 20:25:00 -0800

    ciou inversion: subtract->divide

--- setup_venv.sh
+++ setup_venv.sh
@@ -6,12 +6,6 @@ python_version="3.9.7"
 # Set up virtual environment
 venv_name="venv"
 
-if [ -n "$1" ]; then
-    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu"${1}"
-else
-    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-fi
-
 # Check if virtualenv is installed
 if ! command -v virtualenv &> /dev/null; then
     echo "virtualenv is not installed. Installing..."
@@ -22,6 +16,12 @@ fi
 # Create and activate virtual environment
 python3 -m venv "$venv_name"
 source "$venv_name/bin/activate"
+
+if [ -n "$1" ]; then
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu"${1}"
+else
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+fi
 
 # Install Python dependencies
 pip3 install --upgrade pip
